2fd1f413f1c3500dd0722e0e53c6893bb3776b3d
EC <chauedwin1995@gmail.com>
2025-06-25 16:04:39 +0800

    simple display

--- OnlyFriends/urls.py
+++ OnlyFriends/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from contacts.views import contact
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contacts/',contact ,name='contacts'), #endpoint, render function in apps.view, {url "apps:views"}
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contact(request):
+    contacts = []
+    
+    return render(request, 'contacts/contact.html', {'contacts':contacts})
